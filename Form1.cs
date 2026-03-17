82ad1b3bd2a99fe61d4557681211d0e24105a28c
hgu0524 <kalistar0524@gmail.com>
2026-03-17 19:52:12 +0900

    점수 시스템 도입, 성공할때마다 버튼 크기 감

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,8 @@ namespace CatchButton
 {
     public partial class Form1 : Form
     {
+        int score = 0; // ⭐ 3단계 추가
+
         public Form1()
         {
             InitializeComponent();
@@ -11,32 +13,36 @@ namespace CatchButton
 
         private void button1_MouseEnter(object sender, EventArgs e)
         {
-            // 1. 난수생성기준비
-            Random rd= new Random();
-            // 2. 가용영역계산(버튼이폼테두리에걸리지않게보호)
-            // ClientSize는타이틀바와테두리를제외한실제흰도화지영역임
-            int maxX= this.ClientSize.Width;
-            int maxY= this.ClientSize.Height;
-            // 3. 랜덤좌표추출(0 ~ 최대가용치사이)
-            int nextX= rd.Next(0, maxX-372);
-            int nextY= rd.Next(0, maxY-158);
-            // 4. 위치할당(새로운Point 객체생성)
-            runbutton.Location= new Point(nextX, nextY);
-            // 5. 시각적피드백(폼제목표시줄에좌표출력)
-            this.Text = $"버튼위치: ({nextX}, {nextY})";
-            
-        
-            Random rand = new Random();
+            // 1. 난수 생성기 준비
+            Random rd = new Random();
 
+            // 2. 가용 영역 계산
+            int maxX = this.ClientSize.Width;
+            int maxY = this.ClientSize.Height;
+
+            // 3. 랜덤 좌표 추출
+            int nextX = rd.Next(0, maxX - 372);
+            int nextY = rd.Next(0, maxY - 158);
+
+            // 4. 위치 이동
+            runbutton.Location = new Point(nextX, nextY);
+
+            // ❌ 좌표 출력은 점수랑 충돌하니까 제거
+            // this.Text = $"버튼위치: ({nextX}, {nextY})";
+
+            // ⭐ 추가된 랜덤 이동 (기존 유지)
+            Random rand = new Random();
             int x = rand.Next(0, this.ClientSize.Width - runbutton.Width);
             int y = rand.Next(0, this.ClientSize.Height - runbutton.Height);
-
             runbutton.Location = new Point(x, y);
 
-            // 도망갈 때 소리
+            // ⭐ 3단계: 점수 감소
+            score -= 10;
+            this.Text = "점수: " + score;
+
+            // 도망 소리
             SoundPlayer player = new SoundPlayer(@"C:\Users\사용자\Downloads\Blop Sound.wav");
             player.Play();
-
         }
 
         private void runbutton_Click(object sender, EventArgs e)
@@ -47,6 +53,17 @@ namespace CatchButton
             // 잡았을 때 소리
             SoundPlayer catchSound = new SoundPlayer(@"C:\Users\사용자\Downloads\Coin 1.wav");
             catchSound.Play();
+
+            // ⭐ 3단계: 점수 증가
+            score += 100;
+            this.Text = "점수: " + score;
+
+            // ⭐ 3단계: 버튼 크기 감소 (난이도)
+            if (runbutton.Width > 20 && runbutton.Height > 20)
+            {
+                runbutton.Width = (int)(runbutton.Width * 0.9);
+                runbutton.Height = (int)(runbutton.Height * 0.9);
+            }
         }
     }
 }
